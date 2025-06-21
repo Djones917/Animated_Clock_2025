eed9d3534c968356854839119c7bfec00530c24c
Derek <nightwing917@hotmailcom>
2025-06-20 19:36:15 -0600

    adding ctx rotate

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function clock() {
     ctx.save(); // SAVE THE DEFAULT STATE
     ctx.clearRect(0, 0, 500, 500);
     ctx.translate(250, 250);
+    ctx.rotate(-Math.PI / 2);
 
     ctx.restore(); // RESTORE THE DEFAULT STATE
 
