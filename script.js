66dcd0eb73b1cb12d878be14406706263ea6603f
Derek <nightwing917@hotmail.com>
2025-12-03 18:54:46 -0700

    adding linewidth

--- script.js
+++ script.js
@@ -62,6 +62,7 @@ function clock() {
     ctx.save();
     ctx.rotate((Math.PI / 6) * hr + (Math.PI / 360) * min + (Math.PI / 21600) * sec);
     ctx.strokeStyle = '#800000';
+    ctx.lineWidth = 14;
     ctx.restore();
 
 
