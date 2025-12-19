b842464da69c80d1bb67fcf1a580912b53b9b3e1
Derek <nightwing917@hotmail.com>
2025-12-18 17:56:42 -0700

    working on min hand

--- script.js
+++ script.js
@@ -73,10 +73,10 @@ function clock() {
     ctx.save();
     ctx.rotate((Math.PI / 30) * min + (Math.PI / 1800) * sec);
     ctx.strokeStyle = '#800000';
-    ctx.lineWidth = 14;
+    ctx.lineWidth = 8;
     ctx.beginPath();
-    ctx.moveTo(-20, 0);
-    ctx.lineTo(80, 0);
+    ctx.moveTo(-28, 0);
+    ctx.lineTo(110, 0);
     ctx.stroke();
     ctx.restore();
 
